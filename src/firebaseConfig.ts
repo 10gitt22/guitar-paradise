6640d43abbae5d6a41fa92509bbcaac5de5b1d80
Yevhen Gitt <gitt22konoplyanka@gmail.com>
2022-05-16 01:33:20 +0300

    fix: update firebase configs

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBziazGxxyh03HV4CfS0UkQi74HQLUpveQ",
+  authDomain: "guitar-paradise-store.firebaseapp.com",
+  projectId: "guitar-paradise-store",
+  storageBucket: "guitar-paradise-store.appspot.com",
+  messagingSenderId: "230049728349",
+  appId: "1:230049728349:web:ff5c3af6a50f2f937c8ab4"
 };
 
 // Initialize Firebase
